565326be3b4152d97aee57c19c14677d183d14e4
alamin <amin.se.aiub@gmail.com>
2023-02-15 14:38:48 +0600

    form submission

---
+++ php/nameCheck.php
@@ -0,0 +1,15 @@
+<?php 
+    //print_r($_GET);
+    $username = $_REQUEST['username']; 
+    $password = $_REQUEST['password']; 
+    
+    if($username == "" && $password == "") {
+        echo "Null value ..";
+    }else{
+        if($username == $password){
+            echo "valid user!";
+        }else{
+            echo "invalid user!";
+        }
+    }
+?>
