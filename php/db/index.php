331577ec9bdebd7fc6e146bd813a42600a19eac8
alamin <amin.se.aiub@gmail.com>
2023-03-22 14:32:16 +0600

    file upload+into DB

---
+++ php/db/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+    $con = mysqli_connect('127.0.0.1', 'root', '', 'webtech');
+    $sql = "select * from users";
+    $result = mysqli_query($con, $sql);
+    //print_r($result);
+    //$user = mysqli_fetch_assoc($result);
+    //print_r($user);
+    
+    while($row = mysqli_fetch_assoc($result)){
+        print_r($row); echo "<br>";
+    }
+
+
+?>
